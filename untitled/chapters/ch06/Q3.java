33a9fd8af90f8b885ce3f1f7014a4564a80f4320
CommRogue <55894360+CommRogue@users.noreply.github.com>
2023-04-06 19:53:59 +0300

    Add Q3 answer

---
+++ untitled/chapters/ch06/Q3.java
@@ -0,0 +1,61 @@
+package ch06;
+
+import java.util.ArrayList;
+
+class Entry <K, V> {
+    private final K key;
+    private V value;
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+    public Entry(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+    public Entry(K key) {
+        this(key, null);
+    }
+}
+class Table <K extends Comparable<K> , V> {
+    private final ArrayList<Entry<K, V>> arrList;
+    public Table() {
+        arrList = new ArrayList<>();
+    }
+    public void setKey(K key, V value) {
+        arrList.add(new Entry<>(key, value));
+    }
+    public V getValue(K key) {
+        for(Entry<K, V> entry : arrList) {
+            if(entry.getKey().compareTo(key) == 0) {
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+    public void removeKey(K key) {
+        for(int i = 0; i < arrList.size(); i++) {
+            if(arrList.get(i).getKey().compareTo(key) == 0) {
+                arrList.remove(i);
+                break;
+            }
+        }
+    }
+}
+
+
+public class Q3{
+    public static void main(String[] args) {
+        Table<Integer, Integer> table = new Table<>();
+        table.setKey(3, 5);
+        System.out.println(table.getValue(3));
+    }
+}
